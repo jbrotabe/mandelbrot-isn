02900c9ddc36309b19974336d79fa950925d54d8
tartempion <tartempion.isn@gmail.com>
2013-05-24 13:06:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function Mandelbrot_iter(cx, cy, maxiter)
   var i;
   var x = 0.0;
   var y = 0.0;
-  for (i = 0; i < maxiter && x*x + y*y <= 4; ++i)
+  for (i = 0; i < maxiter && x*x + y*y <= 10000; ++i)
   {
     var tmp = 2*x*y;
     x = x*x - y*y + cx;
@@ -43,23 +43,36 @@ function Mandelbrot()
       }
       else
       {
-        var c = 3*Math.log(i)/Math.log(iterations - 0);
+        var c = 3*Math.log(i)/Math.log(iterations-1);
+		
         if (c < 1)
         { // exterieur
-          pix[ppos] = 0.5*200*(1-c);
-          pix[ppos+1] = 0;
-          pix[ppos+2] = 0;
+          pix[ppos] = 0*(1-c);
+          pix[ppos+1] = 150*(1-c);
+          pix[ppos+2] = 255*(1-c); 
         }
-        else if (c < 2)
+        else if (c < 1.5)
+        {//bord
+          pix[ppos] =0*(c-1);
+          pix[ppos+1] = 255*(c-1);
+          pix[ppos+2] = 0*(c-1);
+        }
+		else if (c < 2)
         {//bord
           pix[ppos] =255*(c-1);
           pix[ppos+1] = 0*(c-1);
           pix[ppos+2] = 0*(c-1);
         }
+		else if (c < 2.5)
+        {//bord
+          pix[ppos] =0*(c-1);
+          pix[ppos+1] = 0*(c-1);
+          pix[ppos+2] = 255*(c-1);
+        }
         else
         {// ptit trait
-          pix[ppos] = 255;
-          pix[ppos+1] = 255;
+          pix[ppos] = 255*(c-2);
+          pix[ppos+1] = 255*(c-2);
           pix[ppos+2] = 255*(c-2);
         }
       }
