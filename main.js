921b0128429f79ae7f798b996686589917d8c54d
tartempion <tartempion.isn@gmail.com>
2013-05-31 10:30:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,84 +1,52 @@
-function Mandelbrot_iter(cx, cy, maxiter)
-{
-  var i;
-  var x = 0.0;
-  var y = 0.0;
-  for (i = 0; i < maxiter && x*x + y*y <= 4; ++i)
-  {
-    var tmp = 2*x*y;
-    x = x*x - y*y + cx;
-    y = tmp + cy;
-  }
-  return i;
-}
- 
-function Mandelbrot()
-{
-  var width = 900;
-  var height = 600;
-  var cd = document.getElementById('menu');
-  var xmin = -2;
-  var xmax = 1;
-  var ymin = -1;
-  var ymax = 1;
- 
+	function Mandelbrot(){			
+		var canvas = document.getElementById('canvas');
+		var context = canvas.getContext('2d');
+		var xr = context.canvas.width;
+		var yr = context.canvas.height;
+		var image = context.createImageData(xr, yr);
+		var pix = image.data;
+		var xmin = -2; var xmax = 1;
+		var ymin = -1; var ymax = 1;
+		var r0 = 0; var v0 = 0; var b0 = 0;	
+		while(r0 == v0 || r0 == b0 || v0 == b0) 
+			{
+			r0 = Math.pow(2, Math.ceil(Math.random() * 3 + 3));
+			v0 = Math.pow(2, Math.ceil(Math.random() * 3 + 3));
+			b0 = Math.pow(2, Math.ceil(Math.random() * 3 + 3)); 
+			}
 
-  var iterations = parseInt(cd.iterations.value);
-  var ctx = document.getElementById('image').getContext("2d");
-  var img = ctx.getImageData(0, 0, width, height);
-  var pix = img.data;
-  for ( var ix = 0;ix < width; ++ix){
-    for (  var iy = 0; iy < height; ++iy)
-    {
-      var x = xmin + (xmax-xmin)*ix/(width-1);
-      var y = ymin + (ymax-ymin)*iy/(height-1);
-      var i = Mandelbrot_iter(x, y, iterations);
-      var ppos = 4*(900*iy + ix);
-      if (i == iterations)
-      {
-    // interieur
-        pix[ppos] = 0;
-        pix[ppos+1] = 0;
-        pix[ppos+2] = 0;
-      }
-      else
-      {
-        var c = 3*Math.log(i)/Math.log(iterations-1);
-		
-        if (c < 1)
-        { // exterieur
-          pix[ppos] = 0*(1-c);
-          pix[ppos+1] = 150*(1-c);
-          pix[ppos+2] = 255*(1-c); 
-        }
-        else if (c < 1.5)
-        {//bord
-          pix[ppos] =0*(c-1);
-          pix[ppos+1] = 255*(c-1);
-          pix[ppos+2] = 0*(c-1);
-        }
-		else if (c < 2)
-        {//bord
-          pix[ppos] =255*(c-1);
-          pix[ppos+1] = 0*(c-1);
-          pix[ppos+2] = 0*(c-1);
-        }
-		else if (c < 2.5)
-        {//bord
-          pix[ppos] =0*(c-1);
-          pix[ppos+1] = 0*(c-1);
-          pix[ppos+2] = 255*(c-1);
-        }
-        else
-        {// ptit trait
-          pix[ppos] = 255*(c-2);
-          pix[ppos+1] = 255*(c-2);
-          pix[ppos+2] = 255*(c-2);
-        }
-      }
-      pix[ppos+3] = 255;//opacité
-    }
-  ctx.putImageData(img,0,0);
-  }
-}
+		var r1 = 256 / r0; var v1 = 256 / v0; var b1 = 256 / b0;
+		var maxIt = 256;//iteration
+		var x = 0; var y = 0;//coord pix DU POINT(le c dans les formules générales)
+		var zx = 0; var zx0 = 0; var zy = 0;
+		var zx2 = 0; var zy2 = 0;
 
+		for (var ky = 0; ky < yr; ky++)//variable hauteur
+		{
+			y = ymin + (ymax - ymin) * ky / yr;//reperage des pixels, "avancement" en fonction de la taille de l'image
+			
+			for(var kx = 0; kx < xr; kx++)//variable largeur
+			{
+				x = xmin + (xmax - xmin) * kx / xr;
+				zx = x; zy = y;// coordonné de "c"
+				
+				for(var i = 0; i < maxIt; i++)
+				{
+					zx2 = zx * zx; zy2 = zy * zy;//ces 2 lignes determinent la divergence
+					if(zx2 + zy2 > 4) break;
+					zx0 = zx2 - zy2 + x;
+					zy = 2 * zx * zy + y;
+					zx = zx0;
+				}
+				
+				var p = (xr * ky + kx) * 4;
+				pix[p] = i % r0 * r1;    //formule empirique trouvé(possible explication mais pas trouvé)
+				pix[p + 1] = i % v0 * v1; 
+				pix[p + 2] = i % b0 * b1; 
+				pix[p + 3] = 255;          
+			}
+		}
+
+	context.putImageData(image, 0, 0);
+	
+	}
